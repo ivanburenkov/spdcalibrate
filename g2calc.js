e150611ac77bda7faa973a2eb2f74791bf97e297
ivanburenkov <85966174+ivanburenkov@users.noreply.github.com>
2021-07-07 17:04:38 -0400

    Add files via upload

--- g2calc.js
+++ g2calc.js
@@ -1156,9 +1156,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5264048,
+var STACK_BASE = 5264144,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 21168;
+    STACK_MAX = 21264;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
